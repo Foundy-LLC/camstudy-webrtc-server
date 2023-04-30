ac16a58d9cef674843d1dd60cfac296997b11f24
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-04-30 12:04:32 +0900

    Fix module import error

--- src/routing_server_socket.ts
+++ src/routing_server_socket.ts
@@ -1,7 +1,7 @@
 import io, { Socket } from "socket.io-client";
-import * as routingServerProtocol from "./constant/routing_server_protocol";
-import { roomService } from "./service/room_service";
-import { createMediaServerRegisterRequest } from "./constant/config";
+import * as routingServerProtocol from "./constant/routing_server_protocol.js";
+import { roomService } from "./service/room_service.js";
+import { createMediaServerRegisterRequest } from "./constant/config.js";
 
 export const createRoutingServerSocket = (): Socket => {
   const routingServerSocket = io(routingServerProtocol.ROUTING_SERVER_URL);
